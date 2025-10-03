e9f0362d93adab6bbb39f555e26efe2f557e010f
Hrishikesh Alabnur <144805491+RishiDevs3248@users.noreply.github.com>
2025-10-03 10:18:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+let arr = ['applause','boo','gsap','tada','victory','wrong','stop'];
+let div = document.getElementById('buttons')
+let frag = document.createDocumentFragment()
+for(let i=0 ; i<arr.length; i++){
+	let btn = document.createElement('button')
+	let aud = document.createElement('audio')
+	aud.setAttribute('src',`sounds/${arr[i]}.mp3`)
+	aud.setAttribute('id',`${arr[i]}`)
+	btn.setAttribute('class','btn')
+	btn.setAttribute('class','stop')
+	btn.textContent = arr[i]
+	btn.addEventListener('click', () => {
+        arr[i].play();
+    });
+	frag.appendChild(aud)
+	frag.appendChild(btn)
+}
+div.appendChild(frag)
+
+
+
+ // <audio id="myAudio" src="path/to/your/audio.mp3"></audio>
+ //    <button id="playButton">Play Audio</button>
+ // const myAudio = document.getElementById('myAudio');
+ //    const playButton = document.getElementById('playButton');
+
+ //    playButton.addEventListener('click', () => {
+ //        myAudio.play();
+ //    });
